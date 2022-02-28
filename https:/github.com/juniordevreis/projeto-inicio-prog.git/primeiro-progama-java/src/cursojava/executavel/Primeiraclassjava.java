382aae602c976e496ef772a8cfc1ea814b4885d7
junior <junior@kali-dev-junior>
2022-02-28 16:53:56 -0300

    comitando

---
+++ https:/github.com/juniordevreis/projeto-inicio-prog.git/primeiro-progama-java/src/cursojava/executavel/Primeiraclassjava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class Primeiraclassjava {
+	
+	/*main é um metodo auto executavel dentro do java*/
+          public static void main(String[] args) {
+        	  
+        	 /*escreve texto no console*/ 
+			System.out.println("meu primeiro progama em java");
+			System.out.println("java é orientado a objetos");
+		}
+}
+
