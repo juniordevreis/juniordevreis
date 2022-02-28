7ae2f132648ad6a904211cefb227761db5b3d448
junior <junior@kali-dev-junior>
2022-02-28 17:51:29 -0300

    comitando

--- https:/github.com/juniordevreis/projeto-inicio-prog.git/primeiro-progama-java/src/cursojava/executavel/Primeiraclassjava.java
+++ https:/github.com/juniordevreis/projeto-inicio-prog.git/primeiro-progama-java/src/cursojava/executavel/Primeiraclassjava.java
@@ -6,8 +6,9 @@ public class Primeiraclassjava {
           public static void main(String[] args) {
         	  
         	 /*escreve texto no console*/ 
-			System.out.println("meu primeiro progama em java");
+			System.out.println("meuxxxx primeiro progama em java");
 			System.out.println("java é orientado a objetos");
+			System.out.println("java é orientado a objets");
 		}
 }
-
+/*comidedwtaxsndfvdfro*/
